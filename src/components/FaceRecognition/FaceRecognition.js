ea0874db4038ac3bdc6326b59e3d08e355933cd2
Cristhian Benitez <cristhiantilleria@gmail.com>
2021-09-09 12:29:16 -0400

    face recognition boxed fixed

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,27 +3,28 @@ import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageURL, boxes }) => {
   const faceBoxes = boxes;
-  const displayFaceBox = faceBoxes.map((faceBox) => {
-    const { topRow, leftCol, bottomRow, rightCol } = faceBox;
-    return (
-      <div
-        key={faceBox.index}
-        className='bounding-box'
-        style={{
-          top: topRow,
-          right: rightCol,
-          bottom: bottomRow,
-          left: leftCol,
-        }}
-      ></div>
-    );
-  });
 
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='image' src={imageURL} alt='' width='500' height='auto' />
-        {displayFaceBox}
+        {imageURL
+          ? faceBoxes.map((faceBox) => {
+              const { topRow, leftCol, bottomRow, rightCol } = faceBox;
+              return (
+                <div
+                  key={faceBox.index}
+                  className='bounding-box'
+                  style={{
+                    top: topRow,
+                    right: rightCol,
+                    bottom: bottomRow,
+                    left: leftCol,
+                  }}
+                ></div>
+              );
+            })
+          : null}
       </div>
     </div>
   );
